ffb37925954208b6769332cd60d7c4313374cc3c
NAGA-87 <naga.2000.chr5@gmail.com>
2020-12-19 23:03:10 +0900

    ペット紹介機能実装

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,22 @@
 class Animal < ApplicationRecord
+
+  self.inheritance_column = :_type_disabled
   
+  validates :image,              presence: true
+  validates :animal_name,        presence: true
+  validates :current_location,   presence: true
+  validates :animal_age,         presence: true
+  validates :type,               presence: true
+  validates :background,         presence: true
+  validates :personality,        presence: true
+  validates :health,             presence: true
+  validates :transfer_method,    presence: true
+
+  # アクティブハッシュの選択が「---」の時は保存されない
+  validates :male_female_id, :vaccine_id, numericality: { other_than: 1, message: 'Status Select' }
+
+  # アソシエーション
+  belongs_to :user
+  has_one   :order
+  has_one_attached :image
 end
--- db/migrate/20201214145534_create_animals.rb
+++ db/migrate/20201214145534_create_animals.rb
@@ -1,7 +1,17 @@
 class CreateAnimals < ActiveRecord::Migration[6.0]
   def change
     create_table :animals do |t|
-
+      t.string :animal_name,           null: false
+      t.string :current_location, null: false
+      t.string :animal_age,      null: false
+      t.string :type,             null: false
+      t.integer :male_female_id,  null: false
+      t.integer :vaccine_id,      null: false
+      t.text :background,         null: false
+      t.string :personality,      null: false
+      t.string :health,           null: false
+      t.string :transfer_method,  null: false
+      t.references :user,         foreign_key: true
       t.timestamps
     end
   end
